883f55960b97c446204dff3a27804426afd532f2
Abhijeet Singh <152297790+KING-YAMA-2110@users.noreply.github.com>
2024-02-11 23:40:17 +0530

    Delete mysite directory

--- mysite/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def login(request,Krack):
-	return render(request,"Krack.html")
-
-def aboutUs(request):
-	return HttpResponse("This is the about section.")
